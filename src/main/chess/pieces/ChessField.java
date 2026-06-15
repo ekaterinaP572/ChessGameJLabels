2bfae44c919f3574e632d0f0e71f435a6cfa281a
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2026-06-15 11:31:43 +0200

    Removed solutions

--- src/main/chess/pieces/ChessField.java
+++
@@ -1,9 +0,0 @@
-package chess.pieces;
-
-public class ChessField extends ChessSprite {
-    private Color color;
-
-    public ChessField(int x, int y, Color color) {
-        super("resources/Chess_f" + (color == Color.WHITE?"l":"d") + "t60.png", "resources/Chess_fat60.png", x, y);
-    }
-}
