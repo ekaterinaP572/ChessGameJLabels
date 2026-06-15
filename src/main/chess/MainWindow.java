a8e9445888d5901a601e3efac4eeb57490d6b99e
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2026-06-15 15:39:20 +0200

    Renamed files

--- src/main/chess/MainWindow.java
+++ src/main/chess/MainWindow.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 public class MainWindow extends JFrame {
     public MainWindow() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //add(new GamePanelComponents(60));
         add(new DrawGamePanel(60));
         pack();
     }
